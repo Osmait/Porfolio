035068f5ae04e73d19c210dd9f377c89e3c50d31
osmait <saulburgos6@gmail.com>
2023-11-28 18:57:44 -0400

    init

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,5 @@
 export const LINKS = {
-  github: "https://github.com/Ladvace",
-  linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
-  medium: "https://ladvace.medium.com/",
-  discord: "https://discordapp.com/users/163300027618295808",
+  github: "https://github.com/Osmait",
+  linkedin: "https://www.linkedin.com/in/jos%C3%A9-sa%C3%BAl-burgos-35680b244/",
+
 };
